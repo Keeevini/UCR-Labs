5443451ee669e95e45cf5c3250f891982d13f7e4
Kevin Ni <kni005@wch136-24.cs.ucr.edu>
2021-04-11 18:17:51 -0700

    Completed part 1

--- CS120B-Labs/lab-2---intro-to-avr/source/main.c
+++ CS120B-Labs/lab-2---intro-to-avr/source/main.c
@@ -1,8 +1,8 @@
 /*	Author: kni005
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 023
+ *	Assignment: Lab 2  Exercise 1
+ *	Exercise Description: [Garage door light sensor]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -14,24 +14,19 @@
 
 int main(void) {
 	/* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs, initialize to 1s
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 
 	/* Insert your solution below */
-	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	// unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	// unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
 	while (1) {
-		// 1) Read input
-		tmpA = PINA & 0x01;
-		// 2) Perform computation
-		// if PA0 is 1, set PB1PB0 = 01, else = 10
-		if (tmpA == 0x01) { // True if PA0 is 1
-			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01 (clear rightmost 2 bits, then set to 01)
-		} else {
-			tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10 (clear rightmost 2 bits, then set to 10)
-		}	
-		// 3) Write output
-		PORTB = tmpB;	
+		if( (PINA & 0x03) == 0x01) { // True if PA0 = 1 && PA1 = 0
+			PORTB = (PORTB & 0xFC) | 0x01; // Sets PORTB to bbbbbb01 (clear rightmost 2 bits, then set to 01)
+		}
+		else {
+			PORTB = (PORTB & 0xFC) | 0x00; // Sets PORTB to bbbbbb00 (clear rightmost 2 bits, then set to 00)
+		}
 	}
 	return 0;
 }
