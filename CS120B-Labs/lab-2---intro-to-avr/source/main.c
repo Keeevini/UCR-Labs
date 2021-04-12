64f5d1dc8f9b15039e0dd43b1520fc7f9714d648
Kevin Ni <kni005@wch136-24.cs.ucr.edu>
2021-04-11 22:43:11 -0700

    Working on part 4

--- CS120B-Labs/lab-2---intro-to-avr/source/main.c
+++ CS120B-Labs/lab-2---intro-to-avr/source/main.c
@@ -1,8 +1,8 @@
 /*	Author: kni005
  *  Partner(s) Name: 
  *	Lab Section: 023
- *	Assignment: Lab 2  Exercise 3
- *	Exercise Description: [Parking space with full lot indicator]
+ *	Assignment: Lab 2  Exercise 4
+ *	Exercise Description: [Amusement Park Ride Weights]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -15,23 +15,25 @@
 int main(void) {
 	/* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs, initialize to 1s
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
+	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs, initialize to 1s
+	DDRC = 0x00; PORTC = 0xFF; // Configure port C's 8 pins as inputs, initialize to 1s
+	DDRD = 0xFF; PORTD = 0x00; // Configure port D's 8 pins as outputs, initialize to 0s
+
 	/* Insert your solution below */
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpC = 0x00; // Temporary variable to hold the value of C
 	while (1) {
 		tmpA = PINA;
-		int i = 0;
-		unsigned char cntavail = 0x00; // Temporary variable to hold the number of empty spaces
-		for (i = 0; i < 4; ++i) { // loop 4 times
-			if ( (tmpA & 0x01) == 0x00) {
-				++cntavail;	
-			}
-			tmpA = tmpA >> 1;
-		}
-		if (cntavail == 0) // If the parking lot is completely full
-			cntavail = (cntavail & 0x7F) | 0x80; // Sets cntavail to 1bbbbbbbb (clear leftmost 1 bit, then set PC7 to 1)
-		PORTC = cntavail;
+		tmpB = PINB;
+		tmpC = PINC;
+
+		if ( (tmpA + tmpB + tmpC) > 140)
+			PORTD = (PORTD & 0xFE) | 0x01; // Sets PORTD to ddddddd1 (clear right most bit, then set to 1)
+		if ( ((tmpA - tmpC) > 80) || ((tmpC - tmpA) > 80) )
+			PORTD = (PORTD & 0xFD) | 0x2; // Sets PORTD to dddddd1d (clear 2nd from right, then set to 1)
+		
+		PORTD = (PORTD & 0x03) | ((tmpA + tmpB + tmpC) << 2); // Sets PORTD to aaaaaadd (clears 6 left bits, then sets them to average)
 	}
 	return 0;
 }
