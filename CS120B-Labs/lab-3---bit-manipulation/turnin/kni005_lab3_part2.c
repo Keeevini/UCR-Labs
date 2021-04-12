832317b2ba8152237105690075b6432f9b910b8d
Kevin Ni <kni005@wch136-24.cs.ucr.edu>
2021-04-11 20:44:34 -0700

    Completed part 2

--- CS120B-Labs/lab-3---bit-manipulation/turnin/kni005_lab3_part2.c
+++ CS120B-Labs/lab-3---bit-manipulation/turnin/kni005_lab3_part2.c
@@ -1,8 +1,8 @@
 /*	Author: kni005
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Assignment: Lab 3  Exercise 2
+ *	Exercise Description: [Fuel sensor]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
