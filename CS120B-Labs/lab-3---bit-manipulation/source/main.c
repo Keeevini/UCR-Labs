fb83415c41e9fe9ca013ff6df0de12dcaaaabfa1
Kevin Ni <kni005@wch136-24.cs.ucr.edu>
2021-04-11 20:18:13 -0700

    Working on lab3 part 2

--- CS120B-Labs/lab-3---bit-manipulation/source/main.c
+++ CS120B-Labs/lab-3---bit-manipulation/source/main.c
@@ -15,28 +15,29 @@
 int main(void) {
 	/* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs, initialize to 1s
-	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs, initialize to 1s
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
 
 	// Should use a temporary variable for all bit manipulation
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpC = 0x00; // Temporary variable to hold the value of B
 	
 	/* Insert your solution below */
 	while (1) {
-		unsigned char counter = 0x00; // Temporary variable to hold the number of 1s
+		unsigned char mask = 0xDF;
 		unsigned char a;
 		tmpA = PINA;
-		tmpB = PINB;
-		for (a = 0; a < 8; ++a) { // Loop through each pin
-			if ( (tmpA & 0x01) == 0x01)
-				++counter;
-			if ( (tmpB & 0x01) == 0x01)
-				++counter;
-			tmpA = tmpA >> 1;				
-			tmpB = tmpB >> 1;
+		for (a = 0; a < 3; ++a) { // for level PC5 PC4 PC3
+			if ( (tmpA == (a*2 + 1)) || (tmpA == (a*2 + 2)) ) {
+				tmpC = (tmpC & mask) | (32/(a+1));
+			}
+			mask = mask + 16/(a+1);
 		}
-		PORTC = counter;
+		for (a = 0; a < 3; ++a) { // for level PC2 PC 1 PC0
+		}
+		
+		if (tmpA <= 4) // low fuel
+			tmpC = (tmpC & 0xBF) | 0x40; // Sets tmpC to c1cccccc (clear 2nd to left bit, then set PC6 to 1)
+		PORTC = tmpC;
 	}
 	return 0;
 }
