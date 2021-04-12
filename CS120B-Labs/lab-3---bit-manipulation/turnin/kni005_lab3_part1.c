0cb26c95d9fa9c5ab3617264008dc570ce2989ef
Kevin Ni <kni005@wch136-24.cs.ucr.edu>
2021-04-11 19:31:24 -0700

    Completed part 1

---
+++ CS120B-Labs/lab-3---bit-manipulation/turnin/kni005_lab3_part1.c
@@ -0,0 +1,42 @@
+/*	Author: kni005
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab 3  Exercise 1
+ *	Exercise Description: [count all 1's]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs, initialize to 1s
+	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs, initialize to 1s
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
+
+	// Should use a temporary variable for all bit manipulation
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	
+	/* Insert your solution below */
+	while (1) {
+		unsigned char counter = 0x00; // Temporary variable to hold the number of 1s
+		unsigned char a;
+		tmpA = PINA;
+		tmpB = PINB;
+		for (a = 0; a < 8; ++a) { // Loop through each pin
+			if ( (tmpA & 0x01) == 0x01)
+				++counter;
+			if ( (tmpB & 0x01) == 0x01)
+				++counter;
+			tmpA = tmpA >> 1;				
+			tmpB = tmpB >> 1;
+		}
+		PORTC = counter;
+	}
+	return 0;
+}
---
+++ CS120B-Labs/lab-3---bit-manipulation/turnin/kni005_lab3_part1_tests.py
@@ -0,0 +1,51 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'PINA: 0x00 PINB: 0x00 => PORTC: 0x00',
+	'steps': [ {'inputs': [('PINA',0x00),('PINB',0x00)], 'iterations': 5 } ],
+	'expected': [('PORTC',0x00)],
+	},
+	{'description': 'PINA: 0x01 PINB: 0x00 => PORTC: 0x01',
+	'steps': [ {'inputs': [('PINA',0x01),('PINB',0x00)], 'iterations': 5 } ],
+	'expected': [('PORTC',0x01)],
+	},
+	{'description': 'PINA: 0x00 PINB: 0x01 => PORTC: 0x01',
+	'steps': [ {'inputs': [('PINA',0x00),('PINB',0x01)], 'iterations': 5 } ],
+	'expected': [('PORTC',0x01)],
+	},
+	{'description': 'PINA: 0x01 PINB: 0x01 => PORTC: 0x02',
+	'steps': [ {'inputs': [('PINA',0x01),('PINB',0x01)], 'iterations': 5 } ],
+	'expected': [('PORTC',0x02)],
+	},
+	{'description': 'PINA: 0xFF PINB: 0xFF => PORTC: 0x10',
+	'steps': [ {'inputs': [('PINA',0xFF),('PINB',0xFF)], 'iterations': 5 } ],
+	'expected': [('PORTC',0x10)],
+	},
+
+#	{'description': 'This test will run second.',
+#	'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
+#	{'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
+#	{'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
+#	{'inputs': [('PIN',<val>)], 'time': 600}, ],
+#	'expected': [('PORT',<val>)],
+#	},
+	]
+
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+# watch = ['PORTB']
+
