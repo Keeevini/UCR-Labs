bd28bb2566924d5f5f5ecaaecca6388f53c2c0d4
Kevin Ni <kni005@wch136-24.cs.ucr.edu>
2021-04-25 21:02:45 -0700

    Added lab 5

---
+++ CS120B-Labs/lab-5/source/main.c
@@ -0,0 +1,23 @@
+/*	Author: kni005
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0xFF; PORTA = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	PORTA = 0x0F;
+    }
+    return 1;
+}
