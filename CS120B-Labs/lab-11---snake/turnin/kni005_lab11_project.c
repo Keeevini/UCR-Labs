239c0d25eac646a9c6b4cf1e4bd0cfc3e7d9cd16
Kevin Ni <kni005@wch136-24.cs.ucr.edu>
2021-06-11 01:59:49 -0700

    Added the demo link

--- CS120B-Labs/lab-11---snake/turnin/kni005_lab11_project.c
+++ CS120B-Labs/lab-11---snake/turnin/kni005_lab11_project.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo Link: 
+ *	Demo Link: https://drive.google.com/file/d/1N0dExCyKHOptC91-A93lNAM1n6AkXrts/view?usp=sharing
  */
  
 #include <stdlib.h>
